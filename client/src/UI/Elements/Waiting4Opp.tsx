fb801ee66150c6316fb114715804db945d95052a
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-10-13 23:56:45 +0100

    working system

--- client/src/UI/Elements/Waiting4Opp.tsx
+++ client/src/UI/Elements/Waiting4Opp.tsx
@@ -1,10 +1,5 @@
 import { FC } from "react";
-import {
- 
-  disconnect,
-  ConnectedStarknetWindowObject,
-} from "@argent/get-starknet";
-import { UseDojoContext } from "../../DojoContext";
+
 
 
 interface Waiting4OpponentProps {
@@ -12,24 +7,18 @@ interface Waiting4OpponentProps {
  }
 
 
-const Waiting4Opponet: FC<Waiting4OpponentProps> = ({ onWaitClick })  => {
+const Waiting4Opponet: FC<Waiting4OpponentProps> = ()  => {
 
-  const {setMode} = UseDojoContext();
+  
 
 
 
   return (
     <div>
       <div>
-        {/* <h1 className="text-white text-3xl sm:text-4xl font-bold leading-10 pt-[60px] pb-[120px]">
-          Dojo Duel
-        </h1> */}
-
         <div className="text-white pt-[50px]">
           <p>processing.....future development.</p>
         </div>
-
-       
       </div>
     </div>
   );
